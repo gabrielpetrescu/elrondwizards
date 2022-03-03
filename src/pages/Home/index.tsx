ab796e74d2bde1d7c49af84ae57d87400e3dafd6
Gabriel Petrescu <gabriel.petrescu@developdesk.com>
2022-03-03 11:38:20 +0200

    repair swiper functionality

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,13 +9,67 @@ import RoadMap from "components/RoadMap";
 import UseOfFounds from "components/UseOfFounds";
 import Footer from "components/Footer";
 import OurCollection from "components/OurCollection";
+import Buy from "../Dashboard/Buy";
+import NFT from "../NFT";
+import Explore from "../Explore";
+import FirstSlider from "../../components/FirstSlider";
+import SecondSlider from "../../components/SecondSlider";
 // prettier-ignore
 const Home = () => {
-  return (
+
+    const [activeTab, setActiveTab] = React.useState({
+        tab1: true,
+        tab2: false,
+    });
+
+    const handleTabSwitch = (e: any) => {
+        e.preventDefault();
+        const value = e.target.innerHTML;
+        if (value.includes("Collection 1"))
+            setActiveTab({
+                tab1: true,
+                tab2: false,
+
+            });
+        if (value.includes("Collection 2"))
+            setActiveTab({
+                tab1: false,
+                tab2: true,
+
+            });
+    };
+
+
+
+    return (
     <div className="home-page">
         <div className="wrapper-top-bg" style={{ backgroundImage: "url(/Homepage-bg-top.png)" }}>
             <Video />
-            <Banner />
+
+
+            <div className="swiper-section container">
+                <div id="tsum-tabs">
+
+                    <div
+                        className={`nav-nft nft-tab ${activeTab.tab1 ? "tab-active" : ""}`}
+                        onClick={handleTabSwitch}
+                    >
+                        <span>Collection 1</span>
+                    </div>
+                    <div
+                        className={`nav-explore nft-tab ${activeTab.tab2 ? "tab-active" : ""}`}
+                        onClick={handleTabSwitch}
+                    >
+                        <span>Collection 2</span>
+                    </div>
+                </div>
+
+                {(() => {
+                    if (activeTab.tab1) return <FirstSlider />;
+                    if (activeTab.tab2) return <SecondSlider />;
+                })()}
+
+            </div>
         </div>
         <div className="wrapper-top-bg" style={{ backgroundImage: "url(/Homepage-bg-bottom.png)" }}>
             <OurCollection />
--- src/pages/OurStory/index.tsx
+++ src/pages/OurStory/index.tsx
@@ -2,9 +2,34 @@ import * as React from "react";
 import Banner from "../../components/Banner";
 import Footer from "../../components/Footer";
 import About from "../../components/About";
+import FirstSlider from "../../components/FirstSlider";
+import SecondSlider from "../../components/SecondSlider";
 
 // prettier-ignore
 const OurStory = () => {
+
+    const [activeTab, setActiveTab] = React.useState({
+        tab1: true,
+        tab2: false,
+    });
+
+    const handleTabSwitch = (e: any) => {
+        e.preventDefault();
+        const value = e.target.innerHTML;
+        if (value.includes("Collection 1"))
+            setActiveTab({
+                tab1: true,
+                tab2: false,
+
+            });
+        if (value.includes("Collection 2"))
+            setActiveTab({
+                tab1: false,
+                tab2: true,
+
+            });
+    };
+
     return (
         <div className="our-story-page">
             <div className="wrapper-top-bg" style={{ backgroundImage: "url(/Homepage-bg-top.png)" }}>
@@ -12,7 +37,29 @@ const OurStory = () => {
                 <p>TEXT HERE</p>
             </div>
             <div className="wrapper-top-bg" style={{ backgroundImage: "url(/Homepage-bg-top.png)" }}>
-                <Banner />
+                <div className="swiper-section container">
+                    <div id="tsum-tabs">
+
+                        <div
+                            className={`nav-nft nft-tab ${activeTab.tab1 ? "tab-active" : ""}`}
+                            onClick={handleTabSwitch}
+                        >
+                            <span>Collection 1</span>
+                        </div>
+                        <div
+                            className={`nav-explore nft-tab ${activeTab.tab2 ? "tab-active" : ""}`}
+                            onClick={handleTabSwitch}
+                        >
+                            <span>Collection 2</span>
+                        </div>
+                    </div>
+
+                    {(() => {
+                        if (activeTab.tab1) return <FirstSlider />;
+                        if (activeTab.tab2) return <SecondSlider />;
+                    })()}
+
+                </div>
             </div>
             <div className="wrapper-top-bg" style={{ backgroundImage: "url(/Homepage-bg-bottom.png)" }}>
                 <About />
