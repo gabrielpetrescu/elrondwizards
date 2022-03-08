f3d2e1d55ee1209da49e47a71d256f431a03be18
robert <robertionutdev@gmail.com>
2022-03-08 15:47:39 +0200

    Menu changes

--- src/components/Layout/Navbar/index.tsx
+++ src/components/Layout/Navbar/index.tsx
@@ -1,23 +1,7 @@
 import React, { useState } from "react";
-import * as Dapp from "@elrondnetwork/dapp";
-import { Link, useHistory } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import * as $ from "jquery";
-import { SidebarData } from "./SidebarData";
 import { slide as Menu } from "react-burger-menu";
-
+import { Link, useHistory } from "react-router-dom";
 const Navbar = () => {
-  const { loggedIn } = Dapp.useContext();
-  const dappLogout = Dapp.useLogout();
-  const history = useHistory();
-
-  const logOut = (e: React.MouseEvent) => {
-    e.preventDefault();
-    dappLogout({ callbackUrl: `${window.location.origin}/` });
-    history.push("/");
-  };
-  const [sidebar, setSidebar] = useState(false);
-  const showSidebar = () => setSidebar(!sidebar);
   return (
     <>
       {/* prettier-ignore */}
@@ -32,8 +16,8 @@ const Navbar = () => {
               </Link>
 
               <Menu right>
-                <a className="menu-item" href="#swiper-collection">Collections</a>
-                <a className="menu-item" href="#team-link">About Us</a>
+                <a className="menu-item" href="/#swiper-collection">Collections</a>
+                <a className="menu-item" href="/#team-link">About Us</a>
                 <a className="menu-item" href="#roadmap-link">Roadmap</a>
                 <a className="menu-item" href="#blog-link">Blog</a>
                 <a className="menu-item" href="#contact-link">Contact</a>
@@ -43,12 +27,12 @@ const Navbar = () => {
 
               <div className="collapse navbar-collapse d-none d-md-block" id="navbarSupportedContent">
                 <ul className="navbar-nav ">
-                  <li><Link to="#swiper-collection" className="nav-link">Collections</Link></li>
-                  <li><Link to="#team-link" className="nav-link">About Us</Link></li>
-                  <li><Link to="#roadmap-link" className="nav-link">Roadmap</Link></li>
-                  <li><Link to="#blog-link" className="nav-link">Blog</Link></li>
-                  <li><Link to="#contact-link" className="nav-link">Contact</Link></li>
-                  <li><Link to="/#" className="nav-link">Whitepaper</Link></li>
+                  <li><a href="/#swiper-collection" className="nav-link">Collections</a></li>
+                  <li><a href="/#team-link" className="nav-link">About Us</a></li>
+                  <li><a href="#roadmap-link" className="nav-link">Roadmap</a></li>
+                  <li><a href="#blog-link" className="nav-link">Blog</a></li>
+                  <li><a href="#contact-link" className="nav-link">Contact</a></li>
+                  <li><a href="/#" className="nav-link">Whitepaper</a></li>
                 </ul>
               </div>
             </nav>
