a2e99360b846788d9147a5ba979b2ac8cd813a62
robert <robertionutdev@gmail.com>
2022-03-17 22:17:11 +0200

    Switch team members

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -135,45 +135,6 @@ const About = () => {
                 </div>
                 <div className="col-xl-3 col-sm-6">
                     <div className="wrapper-card">
-                        <img className="img-fluid person-avatar" src="/Razvan-Sadeanu.png" alt="Razvan Sadeanu"/>
-                        <p className="name-person">
-                            Razvan S.
-                        </p>
-                        <p className="function-person">
-                            CWO
-                        </p>
-                        <div className="row social-icons-person">
-                            <div className="col-12 text-left">
-
-                                <Accordion>
-                                    <Accordion.Item eventKey="2" className={style3}>
-                                        <Accordion.Body>
-                                            <p>Crypto is my hobby, programming is my passion. Perfectionist by nature, I want to perfectly adapt technology to help nature.</p>
-                                        </Accordion.Body>
-                                        <Accordion.Header  onClick={changeStyle3}>
-                                            <a target="_blank" rel="noreferrer"  href="https://twitter.com/razvansad?s=11">
-                                                <img src="/twitter-icon.svg" alt="twitter" className="img-fluid"/>
-                                            </a>
-                                            <a target="_blank" rel="noreferrer"  href="https://ro.linkedin.com/in/razvan-sadeanu-b81208173">
-                                                <img src="/linkedin-icon.svg" alt="linkedin" className="img-fluid"/>
-                                            </a>
-
-                                            <svg className="arrow-expand" width="38" height="38" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg" >
-                                                <path fill="#CA3ADA" d="M33 22.0001L33.7071 21.293L34.4142 22.0001L33.7071 22.7072L33 22.0001ZM11 23.0001C10.4477 23.0001 10 22.5524 10 22.0001C10 21.4478 10.4477 21.0001 11 21.0001V23.0001ZM26.3738 13.9596L33.7071 21.293L32.2929 22.7072L24.9596 15.3739L26.3738 13.9596ZM33.7071 22.7072L26.3738 30.0405L24.9596 28.6263L32.2929 21.293L33.7071 22.7072ZM33 23.0001L11 23.0001V21.0001L33 21.0001V23.0001Z" >
-
-                                                </path>
-                                            </svg>
-
-                                        </Accordion.Header>
-                                    </Accordion.Item>
-                                </Accordion>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-xl-3 col-sm-6">
-                    <div className="wrapper-card">
                         <img className="img-fluid person-avatar" src="/Flavius-Eremia.png" alt="Flavius Eremia"/>
                         <p className="name-person">
                             Flavius E.
@@ -213,6 +174,46 @@ const About = () => {
 
                     </div>
                 </div>
+                <div className="col-xl-3 col-sm-6">
+                    <div className="wrapper-card">
+                        <img className="img-fluid person-avatar" src="/Razvan-Sadeanu.png" alt="Razvan Sadeanu"/>
+                        <p className="name-person">
+                            Razvan S.
+                        </p>
+                        <p className="function-person">
+                            CWO
+                        </p>
+                        <div className="row social-icons-person">
+                            <div className="col-12 text-left">
+
+                                <Accordion>
+                                    <Accordion.Item eventKey="2" className={style3}>
+                                        <Accordion.Body>
+                                            <p>Crypto is my hobby, programming is my passion. Perfectionist by nature, I want to perfectly adapt technology to help nature.</p>
+                                        </Accordion.Body>
+                                        <Accordion.Header  onClick={changeStyle3}>
+                                            <a target="_blank" rel="noreferrer"  href="https://twitter.com/razvansad?s=11">
+                                                <img src="/twitter-icon.svg" alt="twitter" className="img-fluid"/>
+                                            </a>
+                                            <a target="_blank" rel="noreferrer"  href="https://ro.linkedin.com/in/razvan-sadeanu-b81208173">
+                                                <img src="/linkedin-icon.svg" alt="linkedin" className="img-fluid"/>
+                                            </a>
+
+                                            <svg className="arrow-expand" width="38" height="38" viewBox="0 0 44 44" fill="none" xmlns="http://www.w3.org/2000/svg" >
+                                                <path fill="#CA3ADA" d="M33 22.0001L33.7071 21.293L34.4142 22.0001L33.7071 22.7072L33 22.0001ZM11 23.0001C10.4477 23.0001 10 22.5524 10 22.0001C10 21.4478 10.4477 21.0001 11 21.0001V23.0001ZM26.3738 13.9596L33.7071 21.293L32.2929 22.7072L24.9596 15.3739L26.3738 13.9596ZM33.7071 22.7072L26.3738 30.0405L24.9596 28.6263L32.2929 21.293L33.7071 22.7072ZM33 23.0001L11 23.0001V21.0001L33 21.0001V23.0001Z" >
+
+                                                </path>
+                                            </svg>
+
+                                        </Accordion.Header>
+                                    </Accordion.Item>
+                                </Accordion>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
 
             </div>
         </div>
