3b9f5ee6eae754ec4e55c362a6504bdb68030f57
robert <robertionutdev@gmail.com>
2022-03-21 00:38:49 +0200

    Change balls

--- src/components/OurCollection/index.tsx
+++ src/components/OurCollection/index.tsx
@@ -24,15 +24,82 @@ const OurCollection = () => {
                 </div>
                 <div className="col-lg-6 position-relative balls-collection text-center">
 
+                    <div className=" blob ball-top">
+                        <div className="bubble-text">
+                            <h4>0.75 <span>EGLD</span></h4>
+                            <h5>Minting Price</h5>
+                        </div>
+                        <img className="img-fluid" src="/ball-top.svg" alt="bubbles"/>
+                    </div>
+                    <div className=" blob ball-bottom">
+                        <div className="bubble-text">
+                            <h4>1110</h4>
+                            <h5>Items in Collection</h5>
+                        </div>
 
-                    <img className="img-fluid blob ball-top" src="/ball-top.svg" alt="bubbles"/>
-                    <img className="img-fluid blob ball-bottom" src="/ball-bottom.svg" alt="bubbles"/>
-                    <img className="img-fluid blob ball-left" src="/ball-left.png" alt="bubbles"/>
-                    <img className="img-fluid blob ball-right" src="/ball-right.png" alt="bubbles"/>
+                        <img className="img-fluid" src="/ball-bottom.svg" alt="bubbles"/>
+                    </div>
+                    <div className=" blob ball-left">
+                        <div className="bubble-text">
+                            <h4>2223</h4>
+                            <h5>Left to mint</h5>
+                        </div>
+                        <img className="img-fluid" src="/ball-left.png" alt="bubbles"/>
+                    </div>
+                    <div className=" blob ball-right">
+                        <div className="bubble-text">
+                            <h4>3333</h4>
+                            <h5>Total Supply</h5>
+                        </div>
+
+                        <img className="img-fluid" src="/ball-right.png" alt="bubbles"/>
+                    </div>
 
 
+                    <div className="balls-mobile">
+                        <div className="row d-flex d-md-none">
+                            <div className="col-6">
+                                <div className="wrap-ball-mobile ballm-1">
+                                    <div className="wrap-text-mobile">
+                                        <h4>0.75 <span>EGLD</span></h4>
+                                        <h5>Minting Price</h5>
+                                    </div>
 
-                    <img className="img-fluid d-block d-md-none" src="/balls-grouped.png" alt="bubbles"/>
+                                    <img className="img-fluid" src="/ball-left.png" alt="bubbles"/>
+                                </div>
+                            </div>
+                            <div className="col-6">
+                                <div className="wrap-ball-mobile ballm-2">
+                                    <div className="wrap-text-mobile">
+                                        <h4>1110</h4>
+                                        <h5>Items in Collection</h5>
+                                    </div>
+
+                                    <img className="img-fluid" src="/ball-left.png" alt="bubbles"/>
+                                </div>
+                            </div>
+                            <div className="col-6">
+                                <div className="wrap-ball-mobile ballm-3">
+                                    <div className="wrap-text-mobile">
+                                        <h4>2223</h4>
+                                        <h5>Left to mint</h5>
+                                    </div>
+
+                                    <img className="img-fluid" src="/ball-left.png" alt="bubbles"/>
+                                </div>
+                            </div>
+                            <div className="col-6">
+                                <div className="wrap-ball-mobile ballm-4">
+                                    <div className="wrap-text-mobile">
+                                        <h4>3333</h4>
+                                        <h5>Total Supply</h5>
+                                    </div>
+
+                                    <img className="img-fluid" src="/ball-left.png" alt="bubbles"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
